b7cbf255b0cdbea13f8298364a107a35166caa09
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2022-01-05 14:17:35 +0100

    erster component test ohne abhängigkeit

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,18 +9,67 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      declarations: [HomeComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // SETUP
+    fixture.detectChanges(); // ONINIT
     expect(component).toBeTruthy();
   });
+
+  it('should have initial count of 0', () => {
+    fixture.detectChanges();
+
+    expect(component.count).toEqual(0);
+
+    const counterElem = fixture.debugElement.query(By.css('[data-testid="counter"]'));
+    expect(counterElem.nativeElement.textContent).toContain('Counter: 0');
+
+    const decreaseButton = fixture.debugElement.query(By.css('[data-testid="btn-decrease"]'));
+    expect(decreaseButton.properties['disabled']).toBeTruthy();
+
+    expect(decreaseButton.attributes['id']).toEqual('decrease-button');
+  });
+
+  it('should increase count by one if button clicked', () => {
+    fixture.detectChanges(); // Change Detection + OnInit
+
+    expect(component.count).toEqual(0);
+
+    const increaseButton = fixture.debugElement.query(By.css('[data-testid="btn-increase"]'));
+    increaseButton.triggerEventHandler('click', null);
+
+    expect(component.count).toEqual(1);
+
+    fixture.detectChanges(); // Change Detection
+
+    const counterElem = fixture.debugElement.query(By.css('[data-testid="counter"]'));
+    expect(counterElem.nativeElement.textContent).toContain('Counter: 1');
+  });
+
+  it('should decrease count by one if button clicked', () => {
+    component.count = 3;
+    fixture.detectChanges(); // Change Detection + OnInit
+
+    expect(component.count).toEqual(3);
+
+    const decreaseButton = fixture.debugElement.query(By.css('[data-testid="btn-decrease"]'));
+    decreaseButton.triggerEventHandler('click', null);
+
+    expect(component.count).toEqual(2);
+
+    fixture.detectChanges(); // Change Detection
+
+    const counterElem = fixture.debugElement.query(By.css('[data-testid="counter"]'));
+    expect(counterElem.nativeElement.textContent).toContain('Counter: 2');
+  });
+
+  it('should not decrease count if count is 0', () => {});
 });
