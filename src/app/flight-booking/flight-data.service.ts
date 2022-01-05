e04b25e87fddd9e75f07818560ca3a76841d6989
Stephanie Katterwe <stephanie.katterwe@dpdhl.com>
2022-01-05 14:23:03 +0100

    data service example and look-ahead component with unsubscribe

---
+++ src/app/flight-booking/flight-data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { Flight } from '../entities';
+import { FlightService } from './flight-search/flight.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FlightDataService {
+  private _flights$: ReplaySubject<Flight[]> = new ReplaySubject<Flight[]>(1);
+
+  constructor(private flightService: FlightService) {}
+
+  loadFlights(from: string) {
+    this._flights$.next(null);
+    return this.flightService.search(from).subscribe((val) => this._flights$.next(val));
+  }
+
+  get flights$(): Observable<Flight[]> {
+    return this._flights$.asObservable();
+  }
+}
--- src/app/flight-booking/flight-lookahead/flight-lookahead.component.ts
+++ src/app/flight-booking/flight-lookahead/flight-lookahead.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, map, switchMap, tap } from 'rxjs/operators';
 import { FlightService } from '../flight-search/flight.service';
-import { combineLatest, timer } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable, ReplaySubject, Subject, Subscription, timer } from 'rxjs';
 import { Flight } from '../../entities';
 
 @Component({
@@ -10,32 +10,78 @@ import { Flight } from '../../entities';
   templateUrl: './flight-lookahead.component.html',
   styleUrls: ['./flight-lookahead.component.css'],
 })
-export class FlightLookaheadComponent implements OnInit {
+export class FlightLookaheadComponent implements OnInit, OnDestroy {
   formControl: FormControl;
 
   flights: Flight[];
   loading = false;
 
-  constructor(private fb: FormBuilder, private flightService: FlightService) {}
+  flights$: Observable<Flight[]>;
+
+  private subs: Subscription[] = [];
+
+  constructor(private fb: FormBuilder, private flightService: FlightService) {
+    this.testSubjects();
+  }
 
   ngOnInit(): void {
     // this.formControl = new FormControl();
     this.formControl = this.fb.control(null);
 
-    this.formControl.valueChanges
-      .pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        tap(() => (this.loading = true)),
-        switchMap((value: string) => {
-          // return combineLatest(this.flightService.search(value), timer(700)); // DEPRECATED
-          return combineLatest([this.flightService.search(value), timer(400)]);
-        }),
-        map((result: [Flight[], number]) => result[0])
-      )
-      .subscribe((flights) => {
-        this.flights = flights;
-        this.loading = false;
-      });
+    // alternative instead of subscribing manually assign observable to variable and
+    // use that in the template with async pipe
+    //
+    // this.flights$ = this.formControl.valueChanges.pipe(
+    //   debounceTime(200),
+    //   distinctUntilChanged(),
+    //   tap(() => (this.loading = true)),
+    //   switchMap((value: string) => {
+    //     // return combineLatest(this.flightService.search(value), timer(700)); // DEPRECATED
+    //     return combineLatest([this.flightService.search(value), timer(400)]);
+    //   }),
+    //   map((result: [Flight[], number]) => result[0]),
+    //   tap(() => (this.loading = false))
+    // );
+
+    this.subs.push(
+      this.formControl.valueChanges
+        .pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          tap(() => (this.loading = true)),
+          switchMap((value: string) => {
+            // return combineLatest(this.flightService.search(value), timer(700)); // DEPRECATED
+            return combineLatest([this.flightService.search(value), timer(400)]);
+          }),
+          map((result: [Flight[], number]) => result[0])
+        )
+        .subscribe((flights) => {
+          this.flights = flights;
+          this.loading = false;
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.forEach((s) => s.unsubscribe());
+  }
+
+  testSubjects() {
+    const subject$ = new Subject();
+
+    subject$.next('HELLO');
+    this.subs.push(subject$.subscribe((value) => console.log('SUBJECT', value)));
+    subject$.next('WORLD');
+
+    const behaviourSubject = new BehaviorSubject('INITIAL');
+    behaviourSubject.next('HELLO');
+    this.subs.push(behaviourSubject.subscribe((value) => console.log('BEHAVIOR_SUBJECT', value)));
+    behaviourSubject.next('WORLD');
+
+    const replaySubject$ = new ReplaySubject();
+    replaySubject$.next('INITIAL');
+    replaySubject$.next('HELLO');
+    this.subs.push(replaySubject$.subscribe((value) => console.log('REPLAY_SUBJECT', value)));
+    replaySubject$.next('WORLD');
   }
 }
